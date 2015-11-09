35e5c354f4a0935af30464dec1e31c0c0204ad23
wanderlast <liannelouie@gmail.com>
2015-11-09 02:23:20 -0800

    updated liannel.cpp with overloaded func definitions for treasure generation

--- lianne/lianneL.cpp
+++ lianne/lianneL.cpp
@@ -121,34 +121,38 @@ void treasureGeneration(int i)
 		if (treasure[i].pos[0] == player.pos[0][0] &&
 				treasure[i].pos[1] == player.pos[0][1]) {
 			treasure[i].status = 0;
+      test = -1;
 		} else {
       //not overlapping, check next
       test = 1;
     }
 		
+    //check to see if it's overlapping with other treasure
 		if (test == 1) {
       int x = treasure[i].pos[0];
       int y = treasure[i].pos[1];
-			//check to see if it's overlapping with other treasure
 			for (int j = 0; j < i; j++){
 			  if (x == treasure[j].pos[0] && y == treasure[j].pos[1]){
           //collision occurred
           treasure[i].status = 0;
-          test = 0; //return to the first test
+          test = -1; //return to the first test
           break;
         }
 			}
+      //so if it passed through all values without changing to -1
       if (test == 1){
         test = 2;
       }
 		}
-		
+    
+    //check for overlap with walls		
 		if (test == 2) {
-			//check for overlap with walls
+      int x = treasure[i].pos[0];
+      int y = treasure[i].pos[1];
 		} 
 		
 		//this is i only if it passed all of the other tests
-		if ( test == 0 )
+		if ( test == 3 )
 		{
 			treasure[i].status = 1; //generate the treasure, no collisions occurred
 		}
