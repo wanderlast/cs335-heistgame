6821249f82b26213772519b4429f61ebf75d1b5b
kboberg <kboberg@sleipnir.cs.csubak.edu>
2015-11-12 12:10:10 -0800

    Update kevinB.cpp

--- kevinB.cpp
+++ kevinB.cpp
@@ -16,90 +16,9 @@ bool myWall = false;
 
 using namespace std;
 
-void getGridCenter(const int i, const int j, int cent[2])
-{
-        //This function can be optimized, and made more generic.
-        int b2 = boardDim/2;
-        int screenCenter[2] = {xres/2, yres/2};
-        int s0 = screenCenter[0];
-        int s1 = screenCenter[1];
-        int bq;
-        //quad upper-left corner
-        int quad[2];
-        //bq is the width of one grid section
-
-        //map();
-        bq = (boardDim / gridDim);
-
-
-        //-------------------------------------
-        //because y dimension is bottom-to-top in OpenGL.
-        int i1 = gridDim - i - 1;
-        quad[0] = s0-b2;
-        quad[1] = s1-b2;
-        cent[0] = quad[0] + bq/2;
-        cent[1] = quad[1] + bq/2;
-        cent[0] += (bq * j);
-        cent[1] += (bq * i1);
-}
-
 void physics(void)
 {
     initWall();
-        /*//
-        //
-        //Is it time to move the player?
-        //move the player segments...
-        int headpos[2];
-        //save the head position.
-        headpos[0] = player.pos[0][0];
-        headpos[1] = player.pos[0][1];
-
-        // save
-        //player.direction:
-        //0=down
-        //1=left
-        //2=up
-        //3=right
-        // switch(player.direction) {
-                // case DIRECTION_DOWN:  player.pos[0][1] += 1; break;
-                // case DIRECTION_LEFT:  player.pos[0][0] -= 1; break;
-                // case DIRECTION_UP:    player.pos[0][1] -= 1; break;
-                // case DIRECTION_RIGHT: player.pos[0][0] += 1; break;
-        // }
-
-
-        //
-        //did the player get the treasure
-        if (headpos[0] == treasure.pos[0] && headpos[1] == treasure.pos[1]) {
-                //new position for treasure...
-                int collision=0;
-                int ntries=0;
-                while(1) {
-                        treasure.pos[0] = rand() % gridDim;
-                        //cout << treasure.pos[0] << endl;
-                        treasure.pos[1] = rand() % gridDim;
-                        //cout << treasure.pos[0] << endl;
-                        collision=0;
-                        createSound();
-                        if (treasure.pos[0] == player.pos[0][0] &&
-                                        treasure.pos[1] == player.pos[0][1]) {
-                                        collision=1;
-                                        cleanupSound();
-                                        break;
-                                }
-                        if (!collision) break;
-                        if (++ntries > 1000000) break;
-                }
-                Log("new treasure: %i %i\n",treasure.pos[0],treasure.pos[1]);
-                cout << "treasure collected KB" << endl;
-                if(again == true) {
-        treasure.pos[0] = 6;
-        treasure.pos[1] = 5;
-        again = false;
-                }
-                return;
-        }*/
 }
 
 void initWall(void)
