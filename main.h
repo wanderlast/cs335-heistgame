dd11725d29cad25f646f00e478a752be9e664082
sergio <sergiosplat@aol.com>
2015-11-17 15:44:40 -0800

    Fixed the doubles in code

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #define MAX_GRID 80
 
 typedef struct t_grid {
@@ -38,41 +38,4 @@ extern int soundNum;
 //extern void checkMouse(XEvent *e);
 //extern void checkKeys(XEvent *e);
 //extern int done;
-=======
-#define MAX_GRID 80
 
-typedef struct t_grid {
-    int status;
-    float color[4];
-} Grid;
-
-typedef struct t_treasure {
-    int status; //whether it has spawned
-    int pos[2]; //position (x,y)
-    int maxCount; //max # of treasures to spawn
-    int maxRate; //max treasure spawn rate
-    int increase; //how many treasures should spawn next
-    int type; // coin = 1, jewel = 2
-} Treasure;
-
-extern int xres;
-extern int yres;
-extern Grid grid[MAX_GRID][MAX_GRID];
-extern Treasure treasure[];
-extern int done;
-extern int gridDim;
-extern int boardDim;
-extern int gameover;
-extern int winner;
-extern int score;
-extern void init(void);
-extern void resetGame();
-extern int level;
-extern int treasureScore;
-extern int soundFlag;
-
-//extern void checkResize(XEvent *e);
-//extern void checkMouse(XEvent *e);
-//extern void checkKeys(XEvent *e);
-//extern int done;
->>>>>>> b9a9fb41436cf4b823cb2b18787cc525592bec26
--- sergioA.h
+++ sergioA.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <X11/Xlib.h>
 
 extern int createSound(int);
@@ -8,11 +7,4 @@ extern void infoMenu();
 extern void gamestateMenu();
 extern void checkgamestateMouse(XEvent *e);
 extern int soundNum;
-=======
-extern int createSound(int);
-extern void cleanupSound();
-extern void startMenu();
-extern void infoMenu();
-extern void gamestateMenu();
-extern int soundNum;
->>>>>>> b9a9fb41436cf4b823cb2b18787cc525592bec26
+
