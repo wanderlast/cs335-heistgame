78991badc3d5ad5471dffc879319d7dbd3eb71e2
ryan <ryanjshep@gmail.com>
2015-11-19 11:56:42 -0800

    Final commit.

--- main.h
+++ main.h
@@ -1,3 +1,49 @@
+<<<<<<< HEAD
+#define MAX_GRID 80
+
+typedef struct t_grid {
+        int status;
+        float color[4];
+} Grid;
+
+typedef struct t_treasure {
+        int status; //whether it has spawned
+        int pos[2]; //position (x,y)
+        int maxCount; //max # of treasures to spawn
+        int maxRate; //max treasure spawn rate
+        int increase; //how many treasures should spawn next
+        int type; // coin = 1, jewel = 2
+        int count; //# of treasures generated
+} Treasure;
+
+extern int xres;
+extern int yres;
+extern Grid grid[MAX_GRID][MAX_GRID];
+extern Treasure treasure[];
+extern int done;
+extern int gridDim;
+extern int boardDim;
+extern int gameover;
+extern int winner;
+extern int score;
+extern void init(void);
+extern void resetGame();
+extern int level;
+extern int treasureScore;
+extern int soundFlag;
+extern int credit;
+extern int start;
+extern int timestart;
+extern int soundNum;
+extern int gamemodeSelected;
+extern int gameMode;
+extern void prwall(void);
+//extern void checkResize(XEvent *e);
+//extern void checkMouse(XEvent *e);
+//extern void checkKeys(XEvent *e);
+//extern int done;
+
+=======
 #define MAX_GRID 80
 
 typedef struct t_grid {
@@ -41,3 +87,4 @@ extern void prwall(void);
 //extern void checkKeys(XEvent *e);
 //extern int done;
 
+>>>>>>> 6c7b0f301663bd261600dfa156584bd2a7b50a9a
--- ryanS.h
+++ ryanS.h
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
+#include <X11/Xlib.h>
+
+extern void clock_physics(int);
+extern void clock_animation();
+extern void creditButton();
+extern void checkCreditKeys(XEvent *e);
+extern void checkCreditMouse(XEvent *e);
+extern void creditScreen();
+=======
 extern void clock_physics(int);
 extern void clock_animation();
 extern void creditButton();
 extern void checkCreditKeys();
 extern void checkCreditMouse();
 extern void creditScreen();
+>>>>>>> 6c7b0f301663bd261600dfa156584bd2a7b50a9a
